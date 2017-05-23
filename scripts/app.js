8da48f700a23c9d0341f2a9934abf5817316c1a8
Roman Luntsevich <shipik2008@gmail.com>
2017-05-23 11:55:31 +0300

    Create empty project

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var app = angular.module('site', [
+    'ngRoute', 
+    // 'ui.bootstrap', 
+    // 'hljs', 
+    // 'ngclipboard',
+    // 'duScroll',
+    // 'hl.sticky'
+]);
+
+app.config(['$locationProvider', '$routeProvider', function($locationProvider, routeProvider) {
+    // $locationProvider.html5Mode({
+    //     enabled: true,
+    //     requireBase: false,
+    //     rewriteLinks: true
+    // });
+}]);
+
+app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider.otherwise({
+        redirectTo: "/"
+    });
+}]);
+
