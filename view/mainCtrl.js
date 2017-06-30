198dd24e85afe14af3f7a2220077024a246ebdfd
Roman Luntsevich <shipik2008@gmail.com>
2017-06-30 17:08:01 +0300

    Create grid for table added TD

---
+++ view/mainCtrl.js
@@ -0,0 +1,10 @@
+app.controller('mainCtrl', function($scope){
+
+    // $scope.showSettings = false;
+
+    // $scope.changeShowSetting = function(){
+    //     showSettings = !showSettings;
+    //     return: showSettings;
+    // };
+
+});
